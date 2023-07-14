e1fe3070137051ca0d5f272d4d257e5b4c07f4ab
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-14 18:55:03 +0100

    host

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@-a2@gbp1ujksv3d7166_h-w(d1pob*bu$kdewxs!r*0n11g5k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
