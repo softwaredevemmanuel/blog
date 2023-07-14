98f19df7736c982bb82bf63062fd31076a1789e1
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-14 19:02:21 +0100

    data

--- blog/settings.py
+++ blog/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'event',
-        'USER': 'postgres',
-        'PASSWORD': 'hannah',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'event',
+#         'USER': 'postgres',
+#         'PASSWORD': 'hannah',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
